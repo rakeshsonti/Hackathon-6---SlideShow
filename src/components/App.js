86b90382fb6b120f1cdea55871d237b12506a42d
rambhajansonti <ramsonti0597@gmail.com>
2020-11-24 13:48:17 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,29 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, {Component, useState, useEffect} from "react";
+import '../styles/App.css';
+// import Slide from './Slide';
 
-const App = (props) => {
-  const [index, setIndex] = useState(0);
-  const incrementFn = () => {
-    setIndex(index + 1);
-  };
-  const decrementFn = () => {
-    setIndex(index - 1);
-  };
+const App = (prop) => {
+  const [slide, setSlide] = useState(0);
+
+  const decr = () =>{
+    setSlide(slide - 1);
+  }
+  const incr = () =>{
+    setSlide(slide + 1);
+  }
   return (
-    <>
-      <button data-testid="button-restart" onClick={setIndex(0)} disabled={(index===0)}>
-        Restart
-      </button>
-      <button
-        data-testid="button-prev"
-        onClick={decrementFn}
-        disabled={index === 0}
-      >
-        Prev
-      </button>
-      <button
-        data-testid="button-next"
-        onClick={incrementFn}
-        disabled={index === props.slides.length - 1}
-      >
-        Next
-      </button>
-      <h1 data-testid="title">{props.slides[index].title}</h1>
-      <p data-testid="text">{props.slides[index].text}</p>
-    </>
-  );
-};
+    <div>
+      <button data-testid="button-restart" onClick={() => setSlide(0)} disabled={slide === 0}>Restart</button>
+
+      <button data-testid="button-prev" onClick={decr} disabled={slide === 0}>Prev</button>
+
+      <button data-testid="button-next" onClick={incr} disabled={slide === prop.slides.length-1}>Next</button>
+
+      <h1 data-testid="title">{prop.slides[slide].title}</h1>
+      <p data-testid="text">{prop.slides[slide].text}</p>
+      {/* <Slide title={props.slides[slide].title} text={props.slides[slide].text}></Slide> */}
+
+    </div>
+  )
+}
 export default App;
