528fe73b0569a1b1a9ec8e21843e43dfc0b22e12
rambhajansonti <ramsonti0597@gmail.com>
2020-11-24 13:39:57 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  const incrementFn = () => {
+    setIndex(index + 1);
+  };
+  const decrementFn = () => {
+    setIndex(index - 1);
+  };
   return (
-    <></>
-  )
-}
-
-
+    <>
+      <button data-testid="button-restart" onClick={setIndex(0)} disabled={(index===0)}>
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        onClick={decrementFn}
+        disabled={index === 0}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        onClick={incrementFn}
+        disabled={index === props.slides.length - 1}
+      >
+        Next
+      </button>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
+    </>
+  );
+};
 export default App;
